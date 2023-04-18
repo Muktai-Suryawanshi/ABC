709eab9878dd8ed485e08093e4fb26066c140cc1
Muktai-Suryawanshi <sapanasuryawanashi@gmail.com>
2023-04-18 14:24:02 +0530

    First commit by Muktai

---
+++ src/main/java/com/BikkadIT/AdditionLogic/calculator/Sum.java
@@ -0,0 +1,15 @@
+package com.BikkadIT.AdditionLogic.calculator;
+
+public class Sum {
+		
+		public int sum(int a,int b) {
+			int c=a+b;
+			
+			System.out.println("Addition of a and b="+c);
+			return c;
+		}
+		
+
+	}
+
+
